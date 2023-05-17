2e06f7db5b8de8f744a0ab5b95ac258abd9d686f
Greg M <marainigregory@pm.me>
2023-05-17 12:41:00 +0200

    complete is.authenticated controls for all methods try except IsOwner assign_group for custom migration

--- epicEvents/sales/permissions.py
+++ epicEvents/sales/permissions.py
@@ -1,6 +1,10 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import get_object_or_404
 from rest_framework.permissions import BasePermission
-from .models import Client
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+
+from .models import Client, Event, Contract
 
 
 class IsSaleOrReadOnly(BasePermission):
@@ -9,7 +13,7 @@ class IsSaleOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         # Autoriser les requêtes de lecture à tout le monde
         if request.method in ['GET', 'HEAD', 'OPTIONS']:
-            return True
+            return request.user.is_authenticated
 
         # Vérifier si l'utilisateur est authentifié et a le rôle 'vente'
         if request.user.is_authenticated and request.user.role == 'vente':
@@ -19,57 +23,50 @@ class IsSaleOrReadOnly(BasePermission):
         return False
 
 
-class IsSupport(BasePermission):
-    message = "You are not authorized to perform this action (Support)."
-
-    def has_permission(self, request, view):
-        # Autoriser les requêtes de lecture à tout le monde
-        if request.method in ['GET', 'HEAD', 'OPTIONS']:
-            return True
-
-        # Vérifier si l'utilisateur est authentifié et a le rôle 'support'
-        if request.user.is_authenticated and request.user.role == 'support':
-            return True
-
-        # Les utilisateurs no
-
-
 class IsOwner(BasePermission):
     message = "You are not authorized to perform this action (Owner)."
 
     def has_permission(self, request, view):
         if request.method in ['GET', 'HEAD', 'OPTIONS']:
-            return True
+            return request.user.is_authenticated
 
         client_id = view.kwargs.get('client_id')
-        client = Client.objects.get(client_id=client_id)
+        try:
+            client = Client.objects.get(client_id=client_id)
+        except ObjectDoesNotExist:
+            return False
+
+        if hasattr(view, 'kwargs') and 'pk' in view.kwargs:
+            try:
+                event = Event.objects.get(pk=view.kwargs['pk'])
+                return event.support_contact == request.user or event.client.sales_contact == request.user
+            except ObjectDoesNotExist:
+                pass
+
         return client.sales_contact == request.user
 
     def has_object_permission(self, request, view, obj):
-        # Si la requête est une requête en lecture seule (GET, HEAD, OPTIONS), autoriser tout le monde
         if request.method in ['GET', 'HEAD', 'OPTIONS']:
             return True
 
-        # Si l'utilisateur est authentifié et qu'il est associé au contrat (via le champ sales_contact)
-        if request.user.is_authenticated and obj.sales_contact == request.user:
-            # Récupérer le client associé au contrat
-            client = obj.client
-            # Vérifier que le client est également associé à l'utilisateur (via le champ sales_contact)
-            if client.sales_contact == request.user:
-                return True
+        if isinstance(obj, Event):
+            return obj.support_contact == request.user or obj.client.sales_contact == request.user
 
-        return False
+        return obj.sales_contact == request.user and obj.client.sales_contact == request.user
 
 
 class IsManager(BasePermission):
     def has_permission(self, request, view):
-        return request.user.role == 'Gestion'
+        if request.user.is_authenticated:
+            return request.user.role == 'Gestion'
+        return False
 
 
 class IsClientSalesContact(BasePermission):
     """
     Permission to allow only the sales_contact of a client to create an event for that client.
     """
+
     def has_permission(self, request, view):
         client_id = view.kwargs.get('client_id')
         if client_id is None:
@@ -77,3 +74,27 @@ class IsClientSalesContact(BasePermission):
 
         client = get_object_or_404(Client, client_id=client_id)
         return client.sales_contact == request.user
+
+
+def assign_group_permissions(apps, schema_editor):
+    vente_group, _ = Group.objects.get_or_create(name='Vente')
+    support_group, _ = Group.objects.get_or_create(name='Support')
+
+    vente_content_types = [
+        ContentType.objects.get_for_model(Client),
+        ContentType.objects.get_for_model(Contract),
+        ContentType.objects.get_for_model(Event),
+    ]
+
+    support_content_types = [
+        ContentType.objects.get_for_model(Event),
+    ]
+
+    vente_permissions = Permission.objects.filter(content_type__in=vente_content_types)
+    support_permissions = Permission.objects.filter(content_type__in=support_content_types)
+
+    vente_group.permissions.set(vente_permissions)
+    support_group.permissions.set(support_permissions)
+
+    vente_group.save()
+    support_group.save()
