a1914386f3d29bc18b917be14a33159c509a1287
Greg M <marainigregory@pm.me>
2023-05-17 12:35:55 +0200

    fields changing

--- epicEvents/authentication/admin.py
+++ epicEvents/authentication/admin.py
@@ -1,24 +1,26 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
-class CustomUserAdmin(UserAdmin):
+@admin.register(User)
+class CustomUserAdmin(DjangoUserAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff']
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Custom fields', {'fields': ('role',)}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'password1', 'password2', 'role'),
-        }),
+            'fields': ('username', 'password1', 'password2', 'groups', 'is_staff', 'is_superuser')}
+         ),
     )
-    list_display = ('username', 'role', 'is_staff', 'is_superuser')
-    list_filter = ('role', 'is_staff', 'is_superuser', 'is_active', 'groups')
-    search_fields = ('username',)
+    search_fields = ('username', 'email', 'first_name', 'last_name')
     ordering = ('username',)
+    filter_horizontal = ('groups', 'user_permissions',)
 
-
-admin.site.register(User, CustomUserAdmin)
+# admin.site.unregister(Group)  # Désenregistre le modèle de groupe par défaut
